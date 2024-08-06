663cee1d140c93eeaf8cceeb510dfb9611d0d808
Rishabh Singh <rishabh0739@gmail.com>
2024-08-06 14:23:25 +0530

    bug fixes

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,7 +5,6 @@ export interface Task {
     category: string;
     due_date: string;
     status: 'Incomplete' | 'Complete'; // Assuming status can only be these two values
-    lastUpdate: string;
 }
 
 export interface SearchParams {
